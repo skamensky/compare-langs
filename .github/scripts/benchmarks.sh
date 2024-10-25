c5a59ab0b23a56b4d9bcd8b70d4ddfc9b0a0be1c
Shmuel Kamensky <shmuelkamensky@gmail.com>
2024-10-26 02:18:58 +0300

    fix workflow

--- .github/scripts/benchmarks.sh
+++ .github/scripts/benchmarks.sh
@@ -3,12 +3,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR/../..
 # Build all servers and capture output
-BUILD_OUTPUT=$(./build_all.sh)
+./build_all.sh > build_output.txt
 
 # Run benchmarks and capture output
-TEST_OUTPUT=$(./run_all.sh)
-
-echo "$BUILD_OUTPUT" > build_output.txt
-echo "$TEST_OUTPUT" > test_output.txt
+./run_all.sh > test_output.txt
 
 python $SCRIPT_DIR/update_readme.py
